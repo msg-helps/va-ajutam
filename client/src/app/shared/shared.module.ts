340a1bf09fed6803d51c9dc03b74aac4779cc150
Laszlo Feischmidt <Laszlo.Feischmidt@msg.group>
2020-04-04 13:06:08 +0300

    commit navbar

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,18 +4,22 @@ import {NgModule} from '@angular/core';
 import {InfoCardTextComponent} from './components/info-card/info-card-text/info-card-text.component';
 import {InfoCardComponent} from './components/info-card/info-card.component';
 import {LoadingSpinnerComponent} from './components/loading-spinner/loading-spinner.component';
+import { NavbarComponent } from './components/navbar/navbar-component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [InfoCardComponent, InfoCardTextComponent, LoadingSpinnerComponent],
+  declarations: [InfoCardComponent, InfoCardTextComponent, LoadingSpinnerComponent, NavbarComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   exports: [
     InfoCardComponent,
     InfoCardTextComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    NavbarComponent
   ]
 })
 export class SharedModule {
