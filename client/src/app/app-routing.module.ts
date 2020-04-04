d1e8f2a96bff6cb7c813d5a0e7e8fe4791528f52
antoniomarasescuduran <antonio.marasescu-duran@msg.group>
2020-04-04 10:56:56 +0300

    Merged changes from master.
    3_basic_list

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,19 +2,24 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'pets',
-    loadChildren: './modules/pet-example/pet.module#PetModule'
-  },
-  { path: 'help-requests', loadChildren: () => import('./modules/help-requests/help-requests.module').then(m => m.HelpRequestsModule) },
-  {
-    path: 'request-offer',
-    loadChildren: './modules/request-offer-list/request-offer-list.module#RequestOfferListModule'
-  }
-];
+    {
+      path: 'pets',
+      loadChildren: './modules/pet-example/pet.module#PetModule'
+    },
+    {
+      path: 'help-requests',
+      loadChildren: () => import('./modules/help-requests/help-requests.module').then(m => m.HelpRequestsModule)
+    },
+    {
+      path: 'request-offer',
+      loadChildren: () => import('./modules/request-offer-list/request-offer-list.module').then(m => m.RequestOfferListModule)
+    }
+  ]
+;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
