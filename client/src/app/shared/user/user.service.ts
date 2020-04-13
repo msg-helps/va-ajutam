81d14d378770b5b650345829bd5418b2a7c83161
Laszlo Feischmidt <Laszlo.Feischmidt@msg.group>
2020-04-13 19:43:41 +0300

    commit conflict resolutions

--- client/src/app/shared/user/user.service.ts
+++ client/src/app/shared/user/user.service.ts
@@ -1,47 +1,61 @@
-import {Injectable} from '@angular/core';
-import User from '../model/user.model';
-import {Observable, of} from 'rxjs';
-import {delay} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import User from '../../shared/model/user.model';
+import { Observable, of, observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class UserService {
-
-  constructor() {
-  }
+  
+  constructor(){ }
 
   // Get User data - connection to svr
   getUser(): Observable<User> {
     const users: User[] = [
       {
-        id: 'some-uuid-3',
-        firstName: 'Firstname',
-        lastName: 'Lastname',
-        isAdmin: false,
-        phone: '0757665889',
-        organization: 'OrganizationX',
-        region: 'Cluj'
-      }, {
-        id: 'some-uuid-2',
-        firstName: 'Firstname1',
-        lastName: 'Lastname1',
-        isAdmin: false,
-        phone: '0757665889',
-        organization: 'OrganizationX',
-        region: 'Cluj'
-      }, {
         id: 'some-uuid-1',
-        firstName: 'Firstname2',
-        lastName: 'Lastname2',
+        firstName: "NonAdmin",
+        lastName: "NonBanned",
         isAdmin: false,
-        phone: '0757665889',
-        organization: 'OrganizationX',
-        region: 'Cluj'
+        isBanned: false,
+        phone: "0757665889",
+        organization: "OrganizationX",
+        region: "Cluj" 
+      },{ 
+        id: 'some-uuid-2',
+        firstName: "NonAdmin",
+        lastName: "Banned",
+        isAdmin: false,
+        isBanned: true,
+        phone: "0757665889",
+        organization: "OrganizationX",
+        region: "Cluj" 
+      },{ 
+        id: 'some-uuid-3',
+        firstName: "Admin",
+        lastName: "Banned",
+        isAdmin: true,
+        isBanned: true,
+        phone: "0757665889",
+        organization: "OrganizationX",
+        region: "Cluj" 
+      },{ 
+        id: 'some-uuid-4',
+        firstName: "Admin",
+        lastName: "NonBanned",
+        isAdmin: true,
+        isBanned: false,
+        phone: "0757665889",
+        organization: "OrganizationX",
+        region: "Cluj" 
       }];
 
-
-    const random = 0;
+      
+    const random = Math.floor(Math.random() * (users.length + 1));
+    if (random === users.length) {
+      throw new Error();
+    }
 
     return of(users[random]).pipe(delay(200));
-  }
+  };
 
 }
